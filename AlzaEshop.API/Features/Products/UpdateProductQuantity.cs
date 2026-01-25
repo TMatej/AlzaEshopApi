bd2620fa456aaeaa05e572b77cc7345a27d6aac3
Matej Turek <matej.turek.sk@gmail.com>
2026-01-25 22:06:11 +0100

    feat: UpdateProductQuantityEndpoint was implemented

---
+++ AlzaEshop.API/Features/Products/UpdateProductQuantity.cs
@@ -0,0 +1,76 @@
+﻿using AlzaEshop.API.Common.Database.Contract;
+using AlzaEshop.API.Common.Endpoints;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlzaEshop.API.Features.Products;
+
+public sealed record UpdateProductQuantityRequest
+{
+    public int? Quantity { get; set; }
+}
+
+public sealed record UpdateProductQuantityQuery
+{
+    public Guid Id { get; set; }
+    public int? Quantity { get; set; }
+}
+
+public sealed class UpdateProductQuantityQueryValidator : AbstractValidator<UpdateProductQuantityQuery>
+{
+    public UpdateProductQuantityQueryValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty();
+
+        RuleFor(x => x.Quantity)
+            .NotNull()
+            .GreaterThanOrEqualTo(0);
+    }
+}
+
+public class UpdateProductQuantityEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products/{productId:guid}/quantity", Handle);
+    }
+
+    private static async Task<IResult> Handle(
+        [FromRoute] Guid productId,
+        [FromBody] UpdateProductQuantityRequest request,
+        IValidator<UpdateProductQuantityQuery> validator,
+        IDatabaseContext dbContext,
+        ILogger<UpdateProductQuantityEndpoint> logger,
+        CancellationToken cancellationToken)
+    {
+        var query = new UpdateProductQuantityQuery
+        {
+            Id = productId,
+            Quantity = request.Quantity
+        };
+
+        var validationResult = await validator.ValidateAsync(query, cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            var validationRepresentation = validationResult.ToDictionary();
+            logger.LogWarning("Request validation did not succeed: {@Request}. Validation result: {@ValidationResult}", request, validationRepresentation);
+            return Results.ValidationProblem(validationRepresentation);
+        }
+
+        var product = await dbContext.Products.GetSingleAsync(productId);
+        if (product is null)
+        {
+            return Results.NotFound($"Product with Id: {productId} does not exist.");
+        }
+
+        var originalQuantity = product.Quantity;
+
+        product.Quantity = request.Quantity!.Value;
+        await dbContext.Products.UpdateSingleAsync(product);
+
+        logger.LogInformation("Product quantity was updated from {OriginalProductQuantity} to {NewProductQuantity}", originalQuantity, product.Quantity);
+
+        return Results.Ok();
+    }
+}
