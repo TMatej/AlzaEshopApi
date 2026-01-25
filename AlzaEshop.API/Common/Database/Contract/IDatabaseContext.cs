4641f1d143fe702fcd3deacb76b37c2a582f8f3c
Matej Turek <matej.turek.sk@gmail.com>
2026-01-25 19:03:57 +0100

    feat: data layer abstraction consisting of DatabaseContext and generic Repository with common CRUD async functions was established

---
+++ AlzaEshop.API/Common/Database/Contract/IDatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿namespace AlzaEshop.API.Common.Database.Contract;
+
+/// <summary>
+/// Database abstraction.
+/// </summary>
+public interface IDatabaseContext
+{
+    /// <summary>
+    /// Products repository.
+    /// </summary>
+    IRepository<Product> Products { get; set; }
+}
---
+++ AlzaEshop.API/Common/Database/Contract/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace AlzaEshop.API.Common.Database.Contract;
+
+/// <summary>
+/// Generic repository abstraction with common CRUD async methods.
+/// </summary>
+/// <typeparam name="TEntity"></typeparam>
+public interface IRepository<TEntity>
+    where TEntity : IEntity
+{
+    Task<IList<TEntity>> GetAllAsync();
+    Task<TEntity?> GetSingleAsync(Guid id);
+    Task<TEntity> CreateSingleAsync(TEntity entity);
+    Task UpdateSingleAsync(TEntity entity);
+    Task DeleteSingleAsync(TEntity entity);
+}
